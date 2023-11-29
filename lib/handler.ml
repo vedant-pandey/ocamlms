5c3812124c30363897cf753865d7a5f9bfbf71ff
Vedant Pandey <vedantpandey46@gmail.com>
2023-11-30 00:31:48 +0530

    clean up and add svelte

--- lib/handler.ml
+++ lib/handler.ml
@@ -1,30 +1,7 @@
 open Ppx_yojson_conv_lib.Yojson_conv.Primitives
 open Utils
-open Tyxml.Html
-(**)
-(* let route_list = *)
-(*   [ "/login", Login, Get *)
-(*   ; "/login", Login, Post *)
-(*   ; "/", Home, Get *)
-(*   ; "/register", Register, Get *)
-(*   ; "/profile", Profile, Get *)
-(*   ; "/course", Course, Get *)
-(*   ; "/**", NotFound, Get *)
-(*   ; "/**", NotFound, Post *)
-(*   ] *)
-(* ;; *)
 
-let route_list =
-  [ Get Login
-  ; Post Login
-  ; Get Home
-  ; Get Register
-  ; Get Profile
-  ; Get Course
-  ; Get NotFound
-  ; Post NotFound
-  ]
-;;
+let route_list = [ Post Login; Post NotFound ]
 
 let post_routes req route =
   match route with
@@ -38,20 +15,6 @@ let post_routes req route =
      | _ -> Dream.respond "")
 ;;
 
-let home_page = Utils.build_page ~title:"Home" [ txt "Home" ]
-let register_page = Utils.build_page ~title:"Register" [ txt "Register" ]
-let profile_page = Utils.build_page ~title:"Profile" [ txt "Profile" ]
-let course_page = Utils.build_page ~title:"Course" [ txt "Course" ]
-
-let render req = function
-  | Login -> req |> Login.page
-  | Home -> home_page
-  | Register -> register_page
-  | Profile -> profile_page
-  | Course -> course_page
-  | NotFound -> course_page
-;;
-
 let route_of_page = function
   | Home -> "/"
   | Login -> "/login"
@@ -62,14 +25,9 @@ let route_of_page = function
 ;;
 
 let map_routes verb =
-  let handler_of_verb =
-    match verb with
-    | Get page_type -> page_type |> route_of_page |> Dream.get
-    | Post page_type -> page_type |> route_of_page |> Dream.post
-  in
   match verb with
-  | Get page_type -> handler_of_verb @@ fun req -> Dream.html @@ render req page_type
-  | Post page_type -> handler_of_verb @@ fun req -> post_routes req page_type
+  | Post page_type ->
+    (Dream.post @@ route_of_page page_type) @@ fun req -> post_routes req page_type
 ;;
 
 let routes =
--- lib/utils.ml
+++ lib/utils.ml
@@ -1,5 +1,3 @@
-open Tyxml.Html
-
 type pages =
   | Login
   | Home
@@ -12,20 +10,4 @@ type verb =
   | Get
   | Post
 
-type my_route =
-  | Get of pages
-  | Post of pages
-
-let build_head ~title:title_str ~scripts ~styles () =
-  let scripts = scripts |> List.map @@ fun url -> script ~a:[ a_src url ] @@ txt "" in
-  let styles = styles |> List.map @@ fun url -> link ~rel:[ `Stylesheet ] ~href:url () in
-  scripts @ styles |> head @@ title @@ txt title_str
-;;
-
-let build_page ?(title = "Hello world") ?(scripts = []) ?(styles = []) content =
-  body content
-  |> html @@ build_head ~scripts ~styles ~title ()
-  |> Fmt.str "%a" @@ pp_elt ()
-;;
-
-let build_html page = Fmt.str "%a@." (Tyxml.Html.pp ~indent:true ()) page
+type my_route = Post of pages
